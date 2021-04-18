02ab294afb24900f543838f0f75185f239996d2c
bibeksh101 <48139921+bibeksh101@users.noreply.github.com>
2021-04-17 18:18:27 -0700

    Update Hello.java
    Lorem Ipsum Added 04/17/21

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World");
+        System.out.println("LOREM IPSUM PRINTED HERE!");
         // Write your code here
     }
 }
