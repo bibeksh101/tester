bc0e06c06138e2c930c76c7fbe4fec931c0fca69
bibeksh101 <48139921+bibeksh101@users.noreply.github.com>
2021-04-19 17:30:22 -0700

    Create HelloNew.java
    Hello New Created with Simple print messages.

---
+++ HelloNew.java
@@ -0,0 +1,7 @@
+public class HelloNew {
+    public static void main(String[] args) {
+        System.out.println("NEW, World");
+        System.out.println("NEW NEW PRINTED HERE!");
+        // Write your code here
+    }
+}
