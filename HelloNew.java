5590e0470d974a8c3968dfb894860bbada220342
bibeksh101 <48139921+bibeksh101@users.noreply.github.com>
2021-04-26 17:46:51 -0700

    Update HelloNew.java
    OLA added

--- HelloNew.java
+++ HelloNew.java
@@ -2,6 +2,7 @@ public class HelloNew {
     public static void main(String[] args) {
         System.out.println("NEW, World");
         System.out.println("NEW NEW PRINTED HERE!");
+        System.out.println("OLA!");
         // Write your code here
     }
 }
