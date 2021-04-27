8e0ed05a7d9e5d5019938dabbc049ca595a8f48c
bibeksh101 <48139921+bibeksh101@users.noreply.github.com>
2021-04-26 20:45:07 -0700

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;  // Import the Scanner class
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World");
         System.out.println("LOREM IPSUM PRINTED HERE!");
-        // Write your code here
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter username");
+        
+        String userName = myObj.nextLine();  // Read user input
+        System.out.println("Username is: " + userName);  // Output user input
     }
 }
